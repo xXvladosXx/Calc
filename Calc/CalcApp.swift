b7b688441fab28ffe1e304dfe516883005b3db96
Mateusz Redestowicz <mati.red@gmail.com>
2023-10-03 13:16:17 +0200

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
